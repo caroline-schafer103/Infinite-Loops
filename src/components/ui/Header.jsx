c2b31bdfef630866801eedbd797ba26889f1368c
caroline-schafer103 <cschafer103@gmail.com>
2025-10-23 12:06:12 -0500

    removing search

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -37,12 +37,7 @@ export default function Header() {
 
       {/* Right Side: Search, Cart, Menu */}
       <div className="flex items-center gap-3">
-        {/* Search */}
-        <Button asChild variant="ghost" className="p-2 rounded-full">
-          <Link href="/search">
-            <Search className="w-5 h-5 text-gray-700" />
-          </Link>
-        </Button>
+
 
         {/* Cart */}
         <Button asChild variant="ghost" className="p-2 rounded-full relative">
